83f6565a1cd7cafbda44b3c2bad281b9242758c3
Liam Morley <liam@carpeliam.com>
2011-04-03 17:46:09 -0400

    fleshed out controller tests to test both signed in and signed out

--- spec/controllers/governor/articles_controller_spec.rb
+++ spec/controllers/governor/articles_controller_spec.rb
@@ -9,55 +9,112 @@ module Governor
       @article = Factory(:article, :author => @user)
     end
     context "#index" do
-      it "assigns the correct instance variable" do
+      it "renders the articles page" do
         get :index, :governor_mapping => :articles
         assigns[:articles].should == [@article]
+        response.should render_template(:index)
       end
     end
     
     context "#new" do
-      it "assigns the correct instance variable" do
-        sign_in @user
-        get :new, :governor_mapping => :articles
-        assigns[:article].should be_a ::Article
-        assigns[:article].attributes.should == ::Article.new.attributes
+      context "when signed out" do
+        it "redirects back to the front page" do
+          get :new, :governor_mapping => :articles
+          response.should redirect_to(root_path)
+        end
+      end
+      
+      context "when signed in" do
+        before(:each) { sign_in @user }
+        
+        it "renders the new article page" do
+          get :new, :governor_mapping => :articles
+          assigns[:article].should be_a ::Article
+          assigns[:article].attributes.should == ::Article.new.attributes
+          response.should render_template(:new)
+        end
       end
     end
     
-    %w(show edit).each do |action|
-      context "##{action}" do
-        it "assigns the correct instance variable" do
-          get action, :governor_mapping => :articles, :id => @article.id
+    context "#show" do
+      it "renders the right article" do
+        get :show, :governor_mapping => :articles, :id => @article.id
+        assigns[:article].should == @article
+        response.should render_template(:show)
+      end
+    end
+    
+    context "#edit" do
+      context "when signed out" do
+        it "redirects back to the front page" do
+          get :edit, :governor_mapping => :articles, :id => @article.id
+          response.should redirect_to(root_path)
+        end
+      end
+      
+      context "when signed in" do
+        before(:each) { sign_in @user }
+        it "renders the edit page" do
+          get :edit, :governor_mapping => :articles, :id => @article.id
           assigns[:article].should == @article
+          response.should render_template(:edit)
         end
       end
     end
     
     context "#create" do
-      it "creates a new instance of Article" do
-        sign_in @user
-        post :create, :governor_mapping => :articles
-        assigns[:article].should be_a ::Article
-        assigns[:article].should_not be_a_new_record
-        assigns[:article].author.should == @user
+      context "when signed out" do
+        it "redirects back to the front page" do
+          post :create, :governor_mapping => :articles
+          response.should redirect_to(root_path)
+        end
+      end
+      
+      context "when signed in" do
+        before(:each) { sign_in @user }
+        it "creates a new article and redirects to its page" do
+          post :create, :governor_mapping => :articles
+          assigns[:article].should be_a ::Article
+          assigns[:article].should_not be_a_new_record
+          assigns[:article].author.should == @user
+          response.should redirect_to(assigns[:article])
+        end
       end
     end
     
     context "#update" do
-      it "updates the article" do
-        sign_in @user
-        put :update, :governor_mapping => :articles, :id => @article.id, :article => {:title => 'I am awesome, you are awesome'}
-        assigns[:article].title.should == 'I am awesome, you are awesome'
+      context "when signed out" do
+        it "redirects back to the front page" do
+          put :update, :governor_mapping => :articles, :id => @article.id, :article => {:title => 'I am awesome, you are awesome'}
+          response.should redirect_to(root_path)
+        end
+      end
+      
+      context "when signed in" do
+        before(:each) { sign_in @user }
+        it "updates the article" do
+          put :update, :governor_mapping => :articles, :id => @article.id, :article => {:title => 'I am awesome, you are awesome'}
+          assigns[:article].title.should == 'I am awesome, you are awesome'
+        end
       end
     end
     
     context "#destroy" do
-      it "deletes the article" do
-        sign_in @user
-        delete :destroy, :governor_mapping => :articles, :id => @article.id
-        assigns[:article].should == @article
-        assigns[:article].should be_destroyed
-        ::Article.count.should == 0
+      context "when signed out" do
+        it "redirects back to the front page" do
+          delete :destroy, :governor_mapping => :articles, :id => @article.id
+          response.should redirect_to(root_path)
+        end
+      end
+      
+      context "when signed in" do
+        before(:each) { sign_in @user }
+        it "deletes the article" do
+          delete :destroy, :governor_mapping => :articles, :id => @article.id
+          assigns[:article].should == @article
+          assigns[:article].should be_destroyed
+          ::Article.count.should == 0
+        end
       end
     end
   end
